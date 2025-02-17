e167e9cc31f6b7d2f6f1385a290ea5c166989ea1
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2025-02-17 11:35:26 -0300

    modificando o ambiente virtual

--- taskflow-backend/venv/Scripts/activate.bat
+++ taskflow-backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Landim\Desktop\Estagio 2025\taskflow-backend\venv
+set VIRTUAL_ENV=C:\Users\Landim\Desktop\TaskFlow\taskflow-backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 

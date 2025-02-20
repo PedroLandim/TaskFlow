cab4feafcf64f0863bc7b99cd014c294287975b3
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2025-02-20 13:17:51 -0300

    adicionando o diretorio persistent para o render

--- taskflow-backend/database.py
+++ taskflow-backend/database.py
@@ -3,8 +3,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Define um caminho persistente para o SQLite no Render
-DB_PATH = "/persistent/taskflow.db" if os.getenv("RENDER") else "./taskflow.db"
+
+DB_DIR = "/persistent"
+if os.getenv("RENDER") and not os.path.exists(DB_DIR):
+    os.makedirs(DB_DIR)
+
+DB_PATH = os.path.join(DB_DIR, "taskflow.db") if os.getenv("RENDER") else "./taskflow.db"
+
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})

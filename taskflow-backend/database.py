c0b0d5c5429bf4ed9fd9833b2df31ca75db50ce5
Pedro Landim <86279678+PedroLandim@users.noreply.github.com>
2025-02-20 13:27:06 -0300

    modificando novamene o diretorio persisent para a criação do banco de dados local

--- taskflow-backend/database.py
+++ taskflow-backend/database.py
@@ -4,13 +4,12 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 
-DB_DIR = "/persistent"
-if os.getenv("RENDER") and not os.path.exists(DB_DIR):
-    os.makedirs(DB_DIR)
+# Cria o diretório 'data' dentro do projeto (se não existir)
+DB_DIR = os.path.join(os.path.dirname(__file__), "data")
+os.makedirs(DB_DIR, exist_ok=True)
 
-DB_PATH = os.path.join(DB_DIR, "taskflow.db") if os.getenv("RENDER") else "./taskflow.db"
-
-SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
+# Define o caminho do banco de dados
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{os.path.join(DB_DIR, 'taskflow.db')}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
